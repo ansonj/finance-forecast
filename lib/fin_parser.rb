d347e89bfe46b1c453244b26dd434e2449b8525c
Anson Jablinski <a@ansonj.org>
2016-10-30 13:13:56 -0500

    Test FinParser initialize

--- lib/fin_parser.rb
+++ lib/fin_parser.rb
@@ -1,5 +1,8 @@
 class FinParser
+  attr_reader :input
+
   def initialize(file_name)
+    @input = File.readlines(file_name) unless file_name.nil?
   end
 
   def input_events
--- spec/fin_parser_spec.rb
+++ spec/fin_parser_spec.rb
@@ -2,7 +2,18 @@ require 'spec_helper'
 
 describe FinParser do
   describe 'initialize' do
-    it 'copies the file contents into @input'
+    it 'copies the file contents into @input' do
+      test_file = 'input.txt'
+      File.exist?(test_file).must_equal true
+
+      parser = FinParser.new(test_file)
+      parser.input.wont_be_nil
+    end
+
+    it 'allows nil input' do
+      parser = FinParser.new(nil)
+      parser.input.must_be_nil
+    end
   end
 
   describe 'input_events' do
