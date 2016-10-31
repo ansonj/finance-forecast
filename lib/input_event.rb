4e088f119a2fe98fe05e7d31fe698dd8122b74b1
Anson Jablinski <a@ansonj.org>
2016-10-31 08:03:15 -0500

    Equality tests for InputEvent and subclasses

--- lib/input_event.rb
+++ lib/input_event.rb
@@ -1,2 +1,62 @@
 class InputEvent
+  attr_accessor :name, :amount, :date
+
+  def initialize(name, amount, date)
+    @name = name
+    @amount = amount
+    @date = InputDate.new(date)
+  end
+
+  def type
+    fail 'unknown InputEvent type'
+  end
+
+  def ==(other)
+    return false unless @name == other.name
+    return false unless @amount == other.amount
+    return false unless @date == other.date
+    return false unless self.type == other.type
+    true
+  end
+end
+
+class SalaryEvent < InputEvent
+  def type
+    :salary
+  end
+end
+
+class BillsEvent < InputEvent
+  def type
+    :bills
+  end
+end
+
+class ExtraEvent < InputEvent
+  def type
+    :extra
+  end
+end
+
+class SaveSpendEvent < InputEvent
+  attr_accessor :save_start_date
+
+  def initialize(name, amount, date, start_date)
+    @name = name
+    @amount = amount
+    @date = InputDate.new(date)
+    @save_start_date = InputDate.new(start_date)
+
+    raise 'Dates equal or out of order' if @date <= @save_start_date
+  end
+
+  def type
+    :save_spend
+  end
+
+  def ==(other)
+    return false unless method(:==).super_method.call(self)
+    return false unless @save_start_date == other.save_start_date
+    true
+  end
 end
---
+++ spec/input_event_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+describe InputEvent do
+  describe 'initialize' do
+    it 'works properly' do
+      name = 'Test'
+      amount = 424.2
+      date = '4-2012'
+      e = InputEvent.new(name, amount, date)
+      e.name.must_equal name
+      e.amount.must_equal amount
+      e.date.must_equal InputDate.new(date)
+    end
+
+    it 'has a special case for SaveSpendEvent' do
+      name = 'Trip to Disney World'
+      amount = 4000
+      date = '6-2017'
+      start_date = '8-2016'
+      e = SaveSpendEvent.new(name, amount, date, start_date)
+      e.name.must_equal name
+      e.amount.must_equal amount
+      e.date.must_equal InputDate.new(date)
+      e.save_start_date.must_equal InputDate.new(start_date)
+    end
+
+    it 'fails if SaveSpendEvent is given equal dates' do
+      date = '3-2017'
+      lambda { SaveSpendEvent.new('Name', 123, date, date) }.must_raise RuntimeError
+    end
+
+    it 'fails if SaveSpendEvent is given dates out of order' do
+      event_date = '3-2018'
+      start_date = '3-2017'
+      lambda { SaveSpendEvent.new('Name', 123, start_date, event_date) }.must_raise RuntimeError
+    end
+  end
+
+  describe '==' do
+    it 'detects equality' do
+      name = 'Starting salary'
+      amount = 55000
+      date = '1-2016'
+      e1 = SalaryEvent.new(name, amount, date)
+      e2 = SalaryEvent.new(name, amount, date)
+      e1.must_equal e2
+    end
+
+    it 'requires equal names' do
+      amount = 12
+      date = '12-2012'
+      e1 = BillsEvent.new('asdf', amount, date)
+      e2 = BillsEvent.new('jkl', amount, date)
+      e1.wont_equal e2
+    end
+
+    it 'requires equal amounts' do
+      name = 'Title'
+      date = '11-2011'
+      e1 = ExtraEvent.new(name, 11, date)
+      e2 = ExtraEvent.new(name, 22, date)
+      e1.wont_equal e2
+    end
+
+    it 'requires equal dates' do
+      name = 'Some name'
+      amount = 123.4
+      e1 = BillsEvent.new(name, amount, '3-2017')
+      e2 = BillsEvent.new(name, amount, '4-2017')
+      e1.wont_equal e2
+    end
+
+    it 'requires equal types' do
+      name = 'A good name'
+      amount = 3.14
+      date = '3-2014'
+      e1 = SalaryEvent.new(name, amount, date)
+      e2 = BillsEvent.new(name, amount, date)
+      e1.wont_equal e2
+    end
+
+    it 'requires equal start dates for SaveSpendEvent' do
+      name = 'Buy a new car'
+      amount = 35000
+      date = '12-2019'
+      start_date1 = '1-2019'
+      start_date2 = '2-2019'
+      e1 = SaveSpendEvent.new(name, amount, date, start_date1)
+      e2 = SaveSpendEvent.new(name, amount, date, start_date2)
+      e1.wont_equal e2
+    end
+  end
+end
