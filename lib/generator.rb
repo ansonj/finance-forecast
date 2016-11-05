5e2f8c627f52a235f7fce51392aef95398f7e90f
Anson Jablinski <a@ansonj.org>
2016-11-05 18:05:50 -0500

    Implement Generator output_months

--- lib/generator.rb
+++ lib/generator.rb
@@ -7,6 +7,76 @@ class Generator
   end
 
   def output_months
-    []
+    current_month = earliest_month_in @input_events
+    last_month = latest_month_in @input_events
+
+    checking_balance = 0.0
+    savings_balance = @savings_starting_balance
+    running_income = 0.0
+    running_expenses = 0.0
+
+    output_months = []
+
+    while current_month <= last_month
+      relevant_events = @input_events.select { |e| e.applies_to_date(current_month) }
+
+      output = OutputMonth.new
+      output.date = current_month
+
+      salary = relevant_events.select { |e| e.type == :salary }
+      bills = relevant_events.select { |e| e.type == :bills }
+      extra = relevant_events.select { |e| e.type == :extra }
+      save_spend = relevant_events.select { |e| e.type == :save_spend }
+
+      output.salary_events = salary
+      output.bills_events = bills
+      output.extra_events = extra
+      output.save_spend_events = save_spend
+
+      output.checking_before = checking_balance
+
+      running_income += salary.inject(0) { |sum, e| sum + e.amount }
+      output.income = running_income + extra.select { |e| e.amount > 0 }
+                                            .inject(0) { |sum, e| sum + e.amount }
+
+      running_expenses += bills.inject(0) { |sum, e| sum + e.amount }
+      output.expenses = running_expenses - extra.select { |e| e.amount < 0 }
+                                                .inject(0) { |sum, e| sum + e.amount }
+
+      output.net = output.income - output.expenses
+      output.checking_after = output.checking_before + output.net
+
+      output.move_to_savings = save_spend.select { |e| e.date > current_month }
+                                         .inject(0) { |sum, e| sum + e.saving_increment }
+      output.checking_final = output.checking_after - output.move_to_savings
+
+      output.saving_before = savings_balance
+      output.savings_net = output.move_to_savings - save_spend.select { |e| e.date == current_month }
+                                                              .inject(0) { |sum, e| sum + e.amount }
+      output.saving_after = output.saving_before + output.savings_net
+
+      checking_balance = output.checking_final
+      savings_balance = output.saving_after
+
+      output_months << output
+      current_month = current_month.next_month
+    end
+
+    output_months
+  end
+
+  private def earliest_month_in(events)
+    all_months_in(events).min
+  end
+
+  private def latest_month_in(events)
+    all_months_in(events).max
+  end
+
+  private def all_months_in(events)
+    months = events.map { |e| e.date }
+    months += events.select { |e| e.type == :save_spend }
+                    .map { |e| e.save_start_date }
+    months
   end
 end
--- spec/generator_spec.rb
+++ spec/generator_spec.rb
@@ -10,6 +10,155 @@ describe Generator do
   end
 
   describe 'output_months' do
-    it 'needs to be tested'
+    it 'works in an extremely simple case' do
+      events = [SalaryEvent.new('Test salary', 30_000.00, '1-2016')]
+      save_start = 0.0
+      expected_months = -> {
+        m = OutputMonth.new
+        m.date = InputDate.new('1-2016')
+        m.checking_before = 0
+        m.income = 30_000.0
+        m.expenses = 0
+        m.net = 30_000
+        m.checking_after = 30_000.0
+        m.move_to_savings = 0
+        m.checking_final = 30_000.0
+        m.saving_before = 0
+        m.savings_net = 0
+        m.saving_after = 0
+        m.salary_events = events
+        m.bills_events = []
+        m.extra_events = []
+        m.save_spend_events = []
+        [m]
+      }.call
+
+      months = Generator.new(events, save_start).output_months
+      months.must_equal expected_months
+    end
+
+    it 'works in a basic case' do
+      salary = SalaryEvent.new('Salary', 1_000, '1-2016')
+      bills = BillsEvent.new('Bills', 500, '1-2016')
+      extra = ExtraEvent.new('Get a bonus', 100, '3-2016')
+      events = [salary, bills, extra]
+      save_start = 0.0
+      expected_months = -> {
+        jan = OutputMonth.new
+        jan.date = InputDate.new('1-2016')
+        jan.checking_before = 0
+        jan.income = 1000
+        jan.expenses = 500
+        jan.net = 500
+        jan.checking_after = 500
+        jan.move_to_savings = 0
+        jan.checking_final = 500
+        jan.saving_before = 0
+        jan.savings_net = 0
+        jan.saving_after = 0
+        jan.salary_events = [salary]
+        jan.bills_events = [bills]
+        jan.extra_events = []
+        jan.save_spend_events = []
+        feb = OutputMonth.new
+        feb.date = InputDate.new('2-2016')
+        feb.checking_before = 500
+        feb.income = 1000
+        feb.expenses = 500
+        feb.net = 500
+        feb.checking_after = 1000
+        feb.move_to_savings = 0
+        feb.checking_final = 1000
+        feb.saving_before = 0
+        feb.savings_net = 0
+        feb.saving_after = 0
+        feb.salary_events = []
+        feb.bills_events = []
+        feb.extra_events = []
+        feb.save_spend_events = []
+        mar = OutputMonth.new
+        mar.date = InputDate.new('3-2016')
+        mar.checking_before = 1000
+        mar.income = 1100
+        mar.expenses = 500
+        mar.net = 600
+        mar.checking_after = 1600
+        mar.move_to_savings = 0
+        mar.checking_final = 1600
+        mar.saving_before = 0
+        mar.savings_net = 0
+        mar.saving_after = 0
+        mar.salary_events = []
+        mar.bills_events = []
+        mar.extra_events = [extra]
+        mar.save_spend_events = []
+        [jan, feb, mar]
+      }.call
+
+      months = Generator.new(events, save_start).output_months
+      months.must_equal expected_months
+    end
+
+    it 'works with a SaveSpendEvent' do
+      salary = SalaryEvent.new('Salary', 1_000, '1-2016')
+      bills = BillsEvent.new('Bills', 300, '1-2016')
+      save_spend = SaveSpendEvent.new('Vacation', :travel, 750, '3-2016', '1-2016')
+      events = [salary, bills, save_spend]
+      save_start = 100.0
+      expected_months = -> {
+        jan = OutputMonth.new
+        jan.date = InputDate.new('1-2016')
+        jan.checking_before = 0
+        jan.income = 1000
+        jan.expenses = 300
+        jan.net = 700
+        jan.checking_after = 700
+        jan.move_to_savings = 375
+        jan.checking_final = 325
+        jan.saving_before = 100
+        jan.savings_net = 375
+        jan.saving_after = 475
+        jan.salary_events = [salary]
+        jan.bills_events = [bills]
+        jan.extra_events = []
+        jan.save_spend_events = [save_spend]
+        feb = OutputMonth.new
+        feb.date = InputDate.new('2-2016')
+        feb.checking_before = 325
+        feb.income = 1000
+        feb.expenses = 300
+        feb.net = 700
+        feb.checking_after = 1025
+        feb.move_to_savings = 375
+        feb.checking_final = 650
+        feb.saving_before = 475
+        feb.savings_net = 375
+        feb.saving_after = 850
+        feb.salary_events = []
+        feb.bills_events = []
+        feb.extra_events = []
+        feb.save_spend_events = [save_spend]
+        mar = OutputMonth.new
+        mar.date = InputDate.new('3-2016')
+        mar.checking_before = 650
+        mar.income = 1000
+        mar.expenses = 300
+        mar.net = 700
+        mar.checking_after = 1350
+        mar.move_to_savings = 0
+        mar.checking_final = 1350
+        mar.saving_before = 850
+        mar.savings_net = -750
+        mar.saving_after = 100
+        mar.salary_events = []
+        mar.bills_events = []
+        mar.extra_events = []
+        mar.save_spend_events = [save_spend]
+        [jan, feb, mar]
+      }.call
+
+      months = Generator.new(events, save_start).output_months
+      months.must_equal expected_months
+    end
   end
 end
