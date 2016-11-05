17b3c029f16fc2c4258f65af9f27f8671d566dba
Anson Jablinski <a@ansonj.org>
2016-11-05 16:26:56 -0500

    Group equality tests under a describe block

--- spec/input_date_spec.rb
+++ spec/input_date_spec.rb
@@ -7,47 +7,49 @@ describe InputDate do
     date.year.must_equal 2016
   end
 
-  it 'works with equality' do
-    d1 = InputDate.new('1-2017')
-    d2 = InputDate.new('1-2017')
-    d1.must_equal d2
-  end
+  describe 'Comparable implementation' do
+    it 'works with equality' do
+      d1 = InputDate.new('1-2017')
+      d2 = InputDate.new('1-2017')
+      d1.must_equal d2
+    end
 
-  it 'works with less than' do
-    d1 = InputDate.new('11-2016')
-    d2 = InputDate.new('12-2016')
-    d3 = InputDate.new('1-2017')
+    it 'works with less than' do
+      d1 = InputDate.new('11-2016')
+      d2 = InputDate.new('12-2016')
+      d3 = InputDate.new('1-2017')
 
-    d1.must_be :<, d2
-    d1.must_be :<, d3
-    d2.must_be :<, d3
-  end
+      d1.must_be :<, d2
+      d1.must_be :<, d3
+      d2.must_be :<, d3
+    end
 
-  it 'works with less than or equal' do
-    d1 = InputDate.new('1-2017')
-    d2 = InputDate.new('1-2017')
-    d3 = InputDate.new('2-2017')
+    it 'works with less than or equal' do
+      d1 = InputDate.new('1-2017')
+      d2 = InputDate.new('1-2017')
+      d3 = InputDate.new('2-2017')
 
-    d1.must_be :<=, d2
-    d1.must_be :<=, d3
-  end
+      d1.must_be :<=, d2
+      d1.must_be :<=, d3
+    end
 
-  it 'works with greater than' do
-    d1 = InputDate.new('11-2016')
-    d2 = InputDate.new('12-2016')
-    d3 = InputDate.new('1-2017')
+    it 'works with greater than' do
+      d1 = InputDate.new('11-2016')
+      d2 = InputDate.new('12-2016')
+      d3 = InputDate.new('1-2017')
 
-    d3.must_be :>, d1
-    d3.must_be :>, d2
-    d2.must_be :>, d1
-  end
+      d3.must_be :>, d1
+      d3.must_be :>, d2
+      d2.must_be :>, d1
+    end
 
-  it 'works with greater than or equal' do
-    d1 = InputDate.new('1-2017')
-    d2 = InputDate.new('2-2017')
-    d3 = InputDate.new('2-2017')
+    it 'works with greater than or equal' do
+      d1 = InputDate.new('1-2017')
+      d2 = InputDate.new('2-2017')
+      d3 = InputDate.new('2-2017')
 
-    d3.must_be :>=, d1
-    d3.must_be :>=, d2
+      d3.must_be :>=, d1
+      d3.must_be :>=, d2
+    end
   end
 end
