d089c80403bde58c06f168b58f0a84966ddb914a
Anson Jablinski <a@ansonj.org>
2016-11-05 17:44:35 -0500

    fixup! Add necessary data fields to OutputMonth

--- lib/output_month.rb
+++ lib/output_month.rb
@@ -1,8 +1,8 @@
 class OutputMonth
   attr_accessor :date
   attr_accessor :checking_before, :income, :expenses, :net, :checking_after,
-                :total_save, :checking_final
-  attr_accessor :saving_before, :saving_after
+                :move_to_savings, :checking_final
+  attr_accessor :saving_before, :savings_net, :saving_after
   attr_accessor :salary_events, :bills_events, :extra_events, :save_spend_events
 
   def ==(other)
