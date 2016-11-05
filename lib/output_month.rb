09c500ac55e4e16de92404edd30ab3d433655345
Anson Jablinski <a@ansonj.org>
2016-11-05 16:56:32 -0500

    Add necessary data fields to OutputMonth

--- lib/output_month.rb
+++ lib/output_month.rb
@@ -1,2 +1,7 @@
 class OutputMonth
+  attr_accessor :date
+  attr_accessor :checking_before, :income, :expenses, :net, :checking_after,
+                :move_to_savings, :checking_final
+  attr_accessor :saving_before, :savings_net, :saving_after
+  attr_accessor :salary_events, :bills_events, :extra_events, :save_spend_events
 end
