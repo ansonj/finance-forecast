5ab564af735846160e81a16d6bfae627c262e428
Anson Jablinski <a@ansonj.org>
2016-11-05 17:01:39 -0500

    Add Generator initialization

--- lib/generator.rb
+++ lib/generator.rb
@@ -1,5 +1,9 @@
 class Generator
+  attr_reader :input_events, :savings_starting_balance
+
   def initialize(events, start = 0)
+    @input_events = events
+    @savings_starting_balance = start
   end
 
   def output_months
